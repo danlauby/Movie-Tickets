fd89fd11e09355300ec8901bd387fc46b2de8c89
Dan Lauby and Sarah Leahy <sarah@>
2017-01-24 16:43:34 -0800

    outputs description to movie

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,26 @@ function Ticket(age,movie,time) {
   this.age = age;
   this.movie = movie;
   this.time = time;
-  // this.movies = [];
-}
-function resetFields() {
-  $('input#age').val("");
+  this.movies = [];
 }
 
-// function Movie(name, rating, desc)
+function Movie(movieName, rating, movieDescription) {
+  this.movieName = movieName;
+  this.rating = rating;
+  this.movieDescription = movieDescription;
+}
+
 // $16-full priced adult ticket @ night
 // $8-senior/kids ticket at  night
 // $8-day ticket adult
 // $5-kids day ticket
 
 Ticket.prototype.moviePrice = function(age,time) {
-  if  ((time === "am") && (age >= 50) || (age <= 10)) {
+  if  ((time <= "17:00") && (age >= 50) || (age <= 10)) {
       return ticketPrice - 4;
-    } else if  ((time === "pm") && (age >= 50) || (age <= 10)) {
+    } else if  ((time >= "17:00") && (age >= 50) || (age <= 10)) {
       return ticketPrice - 2;
-    } else if (time === "am") {
+    } else if (time === "11:00") {
       return ticketPrice - 2;
     } else {
       return ticketPrice;
@@ -29,16 +31,49 @@ Ticket.prototype.moviePrice = function(age,time) {
 
 }
 
+Movie.prototype.MovieInfo = function() {
+  return this.movieName + ', ' + this.rating + ', ' + this.movieDescription;
+}
+
+function resetFields() {
+  $('input#age').val("");
+  $('input#time').val("");
+}
+
 $(function() {
   $('form#userInput').submit(function(e) {
-    var movie = $('#movies option:selected').val();
-    var time = $('#time option:selected').val();
+    var selectedMovie = $('#movies option:selected').val();
+    var time = $("input#time").val();
     var age = parseInt($("input#age").val());
-    var newTicket = new Ticket(age,time,movie);
-    $(".output").empty();
-    $(".output").append('<h2>Your movie will cost $</h2>' + newTicket.moviePrice(age,time));
-    e.preventDefault();
 
+    var newTicket = new Ticket(age, selectedMovie, time);
+    var gold = new Movie('gold', 6.2, 'Kenny Wells, a prospector desperate for a lucky break, teams up with a similarly eager geologist and sets off on a journey to find gold in the uncharted jungle of Indonesia.');
+    var sleepless = new Movie('sleepless', 5.6,"A cop with a connection to the criminal underworld scours a nightclub in search of his kidnapped son.")
+    var daughter = new Movie('daughter', 6.8, "The story follows a man who returns home to discover a long-buried family secret, and whose attempts to put things right threaten the lives of those he left home years before." )
+    // var movieData = [
+    //   ["gold", 6.3, "gold dust"]
+    // ];
+    // movieData.forEach(function(movieElements) {
+    //   newTicket.movies.push(
+    //     new Movie(
+    //       moviesElements[0],
+    //       moviesElements[1],
+    //       moviesElements[2]
+    //     )
+    //   });
+    // });
+    newTicket.movies.push(gold, sleepless, daughter);
+    console.log(newTicket);
+
+    $(".output").empty();
+    $(".output").append('<h2>Your movie will cost $ ' + newTicket.moviePrice(age,time) + '</h2>');
+    newTicket.movies.forEach(function(movie) {
+      if (movie.movieName === selectedMovie) {
+        $('.output').append('<h3>' + movie.MovieInfo() + ' </h3>');
+      }
+     });
+
+    e.preventDefault();
     resetFields();
   });
 });
